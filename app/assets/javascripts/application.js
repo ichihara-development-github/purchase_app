29b823113538d7153582ed0890368090c2344fd2
ichihara-github <pasocon.cloud@icloud.com>
2020-01-19 19:27:52 +0000

    correct_js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,15 +22,15 @@ $(function(){
         $(this).next(".dropdown-menu").fadeToggle()
     });
     
-    $(".dropdown-log").click(function(){
+    $(".dropdown-log").on("click", function(){
         $(this).next(".purchacelog").slideToggle()
     });
     
-     $(".fade-menu-toggle").click(function(){
+     $(".fade-menu-toggle").on("click", function(){
         $(this).next(".fade-menu").fadeToggle()
     });
     
-    $('.slide-menu-toggle').click(function(){
+    $('.slide-menu-toggle').on("click", function(){
   
   if($(this).hasClass('active')) {
     $(this).removeClass('active'); 
