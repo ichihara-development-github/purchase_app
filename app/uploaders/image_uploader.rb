f048708650acf971b75c88e5b55701dbf4fe2bc5
ichihara-github <pasocon.cloud@icloud.com>
2020-01-19 18:25:28 +0000

    add_mini_magick

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -21,7 +21,7 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
   
-  # process resize_to_limit: [1200, 5000]
+  process resize_to_limit: [1200, 5000]
   
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
