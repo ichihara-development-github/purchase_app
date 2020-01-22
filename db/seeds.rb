f777c2f8fa944e042c7cc21cce252c3a2946f824
ichihara-github <pasocon.cloud@icloud.com>
2020-01-22 08:07:29 +0000

    correct_seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(name:"user1", email: "user1@example.com", password: "password", introduce:"こんにちはuser1です。ここではbikeshopを運営させていただいてます！",store_id:1,seller: true, admin: true)
-User.create(name:"user2", email: "user2@example.com", password: "password", introduce:"こんにちはuser2です。ここではcakeを運営させていただいてます！",store_id:2, seller: true)
-User.create(name:"user3", email: "user3@example.com", password: "password",introduce:"こんにちはuse3です。ここではprogramshopを運営させていただいてます！",store_id:3, seller: true)
+User.create(name:"admin1", email: "admin1@user.com", password: "password", introduce:"こんにちは管理者１です",seller: true, admin: true)
+User.create(name:"admin2", mail: "admin2@user.com", password: "password", introduce:"こんにちは管理者２です。", admin: true, seller: true)
