ff1ffddaa432f8b4e5a5ae59a79f783c0350be7b
ichihara-development-github <haken.ichihara@icloud.com>
2020-02-21 13:14:14 +0900

    add_models_seeds

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,8 +1,8 @@
 class SessionController < ApplicationController
   def new
-    
+
   end
-  
+
   def create
     user = User.find_by(email: params[:session][:email])
     if user && user.authenticate(params[:session][:password])
@@ -12,32 +12,36 @@ class SessionController < ApplicationController
       flash[:warning] = "メールアドレスとパスワードが一致しません"
       render "new"
     end
-      
+
   end
-  
+
   def guest_login
     user = User.first
     session[:user_id] = user.id
     flash[:success] = "管理者としてログインしました"
     redirect_to productions_path
   end
-  
+
   def guest_user_login
     @user_id = User.last.id
-    user = User.create(name: "user#{@user_id + 1}", email:"user#{@user_id + 1}@user.com", password: "password")
-    session[:guest_id] = user.id
+    if @user_id.nil?
+      user = User.create(name: "user1", email:"user1@user.com", password: "password", admin: true, seller: true)
+    else
+      user = User.create(name: "user#{@user_id + 1}", email:"user#{@user_id + 1}@user.com", password: "password")
+      session[:guest_id] = user.id
+    else
     login user
     redirect_to productions_path
   end
-  
-  
-  
+
+
+
   def destroy
     session[:user_id] = nil
     if session[:guest_id]
       User.find(session[:guest_id]).destroy
       session[:guest_id] = nil
-    end 
+    end
     flash[:success] = "ログアウトしました"
     redirect_to root_path
   end
