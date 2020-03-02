b3c79d0ae87ea85508edcdf6bc9cfb899144826b
ichihara-development-github <haken.ichihara@icloud.com>
2020-03-02 16:19:39 +0900

    corrected_productionsearch_function

--- app/models/production.rb
+++ app/models/production.rb
@@ -1,16 +1,16 @@
 class Production < ApplicationRecord
-    
+
     def self.search(sp)
-     
+
        if sp
            sp.reject! { |key, value| value.empty?}
-           
+
            if sp[:min] or sp[:max]
-               if sp[:min] 
+               if sp[:min]
                    @result = Production.where("price >  ?", sp[:min])
                    sp.reject! { |key, value| key["min"] }
                end
-               if sp[:max] 
+               if sp[:max]
                     @result = Production.where("price <  ?", sp[:max])
                     sp.reject! { |key, value| key["max"]}
                end
@@ -19,16 +19,17 @@ class Production < ApplicationRecord
                     @result = @result.where("#{key.first}": "#{value.first}")
                 end
             end
-           
+
+           @result = Production.where("#{key}": "#{value}")
            sp.each do |key, value|
-               @result = @result.where("#{key}": "#{value}")
+               @result = presult.where("#{key}": "#{value}")
            end
-           
+
            return @result
-           
+
        end
     end
-  
+
    def self.free_search(free_search)
         if free_search
             #検索ワードがあるかどうか
@@ -39,22 +40,22 @@ class Production < ApplicationRecord
             all
         end
    end
-  
-  
+
+
   belongs_to :store
-  
+
   has_many :baskets, dependent: :destroy
   has_many :considered_user, through: :baskets, source: :user
   has_many :purchaceds, dependent: :nullify
   has_many :purchaced_user, through: :purchaceds, source: :user
-  
+
   has_many :comments, dependent: :destroy
-  
+
   validates :name, presence: true, uniqueness: true
   validates :description, presence: true
-  validates :price, presence: true, :numericality => { :greater_than => 0, :less_than => 1000000 } 
- 
-  
+  validates :price, presence: true, :numericality => { :greater_than => 0, :less_than => 1000000 }
+
+
   mount_uploader :main_image, ImageUploader
   mount_uploader :sub_image1, ImageUploader
   mount_uploader :sub_image2, ImageUploader
