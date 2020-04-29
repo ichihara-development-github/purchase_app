b2f5faa25c72055ba0a6a05cd715349f5490e974
ichihara-development-github <haken.ichihara@icloud.com>
2020-04-30 03:12:13 +0900

    corrected_google_api_key

--- app/models/store.rb
+++ app/models/store.rb
@@ -9,9 +9,10 @@ class Store < ApplicationRecord
   mount_uploader :top_image, ImageUploader
 
   after_validation :geocode
+
   private
   def geocode
-    uri = URI.escape("https://maps.googleapis.com/maps/api/geocode/json?address="+self.address.gsub(" ", "")+"#{ENV['GOOGLE_MAP_KEY']}")
+    uri = URI.escape("https://maps.googleapis.com/maps/api/geocode/json?address="+self.address.gsub(" ", "")+"&key=#{ENV['GOOGLE_MAP_KEY']}")
     res = HTTP.get(uri).to_s
     response = JSON.parse(res)
     self.latitude = response["results"][0]["geometry"]["location"]["lat"]
