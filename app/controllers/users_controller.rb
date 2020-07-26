fc3b8c54ec07c82d06934a269e21ea6143efa95b
ichihara-development-github <haken.ichihara@icloud.com>
2020-07-26 19:42:12 +0900

    corrected_before_action_test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :login_user?, only: [:show]
   before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_action :has_store?, only: :management
-  before_action -> { has_authority?(@user) }, only: [:update, :edit, :management]
+  before_action -> { has_authority?(@user) }, only: [:update, :edit]
   before_action :admin_user?, only: [:index, :destroy]
 
   def set_user
