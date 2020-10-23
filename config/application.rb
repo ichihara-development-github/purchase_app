8415b6cac715a4549df7d67e0b01603c2bee478e
ichihara-development-github <haken.ichihara@icloud.com>
2020-10-23 19:22:32 +0900

    add_asynchronous_process_for_get_prices

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module JobMatchingApp
   class Application < Rails::Application
     config.time_zone = 'Tokyo'
     config.i18n.default_locale = :ja
+    config.active_job.queue_adapter = :sidekiq
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
