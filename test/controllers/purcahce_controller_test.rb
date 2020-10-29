da2e24d13f52b21baa72dcca9ee51ad92fa1c914
ichihara-development-github <haken.ichihara@icloud.com>
2020-10-29 16:40:09 +0900

    add_rspec_test

--- test/controllers/purcahce_controller_test.rb
+++ test/controllers/purcahce_controller_test.rb
@@ -1,9 +1,5 @@
 require 'test_helper'
 
 class PurcahceControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get purcahce_new_url
-    assert_response :success
-  end
-
+  
 end
--- test/controllers/stores_controller_test.rb
+++ test/controllers/stores_controller_test.rb
@@ -1,19 +1,5 @@
 require 'test_helper'
 
 class StoresControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get stores_new_url
-    assert_response :success
-  end
-
-  test "should get show" do
-    get stores_show_url
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get stores_edit_url
-    assert_response :success
-  end
-
+  
 end
