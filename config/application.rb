8f2dbca23aefc1984ad9d44ba6834dae7a25bb21
ichihara-development-github <haken.ichihara@icloud.com>
2020-11-08 13:49:44 +0900

    remove_sidekiq_gem_

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,6 @@ module JobMatchingApp
   class Application < Rails::Application
     config.time_zone = 'Tokyo'
     config.i18n.default_locale = :ja
-    config.active_job.queue_adapter = :sidekiq
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
