7f8e42cefc95d95f96ad0b4c8506f0d5877b349c
ichihara-development-github <haken.ichihara@icloud.com>
2020-11-16 02:41:46 +0900

    test

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,16 +7,16 @@ if Rails.env.production?
         config.fog_provider = "fog/aws"
         config.fog_credentials ={
             provider: "AWS",
-            aws_access_key_id: ENV["aws_access_key_id"],
-            aws_secret_access_key: ENV["aws_secret_access_key"],
+            aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+            aws_secret_access_key: ENV["AWS_SECRETE_ACCESS_KEY"],
             region: 'ap-northeast-1'
         }
-        
+
         config.fog_directory = "purchase-app-backet"
         config.asset_host = 'https://purchase-app-backet.s3.amazonaws.com'
-        
+
     end
-    
-    CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/ 
-    
+
+    CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
+
 end
