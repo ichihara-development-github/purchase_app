55594df8c4c12ec57490392490b40d2f3c82e444
ichihara-development-github <haken.ichihara@icloud.com>
2021-03-10 09:44:56 +0900

    updated_aws_config_keys

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,7 +8,7 @@ if Rails.env.production?
         config.fog_credentials ={
             provider: "AWS",
             aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-            aws_secret_access_key: ENV["AWS_SECRETE_ACCESS_KEY"],
+            aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
             region: 'ap-northeast-1'
         }
 
