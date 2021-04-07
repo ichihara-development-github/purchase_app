5d07b68272fb1f7be8caa95adbebd44312497aaf
ichihara-development-github <haken.ichihara@icloud.com>
2021-04-07 13:04:07 +0900

    update_email_host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,11 +1,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  #mailer config
-
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'http://3.128.111.52'
+  host = 'https://ichihara-purchase-app.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
@@ -13,7 +11,7 @@ Rails.application.configure do
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
+    :domain         => 'ichihara-purchase-app.herokuapp.com',
     :enable_starttls_auto => true
   }
 
