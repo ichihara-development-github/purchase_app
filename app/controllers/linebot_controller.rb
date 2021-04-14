c306982a756a0708db8c04a7451233ecff9c6c43
ichihara-development-github <haken.ichihara@icloud.com>
2021-04-14 14:00:20 +0900

    add_carouselmenu

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -74,30 +74,30 @@ class LinebotController < ApplicationController
     body = request.body.read
     events = client.parse_events_from(body)
 
-    events.each do |event|
-      case event
-      when Line::Bot::Event::Message::Postback
-        client.reply_message(event['replyToken'], sticker_list("thanks"))
-        client.reply_message(event['replyToken'], "#{data}")
-      when Line::Bot::Event::Message
-        case event.type
-        when Line::Bot::Event::MessageType::Text
-            message = menu_template
-            client.reply_message(event['replyToken'], message)
-        when Line::Bot::Event::MessageType::Image
-            client.reply_message(event['replyToken'], sticker_list("thanks"))
-            # user_id = event["source"]["userId"]
-            # client.reply_message(event['replyToken'], sticker_list("thanks"))
-        end
-          # push(user_id)
-          # @profile = get_line_UserProfile(user_id)
-          # if check_line_user(user_id)
-          #   send_menue
-          # else
-          #   message = send_greeting
-          #   client.reply_message(event['replyToken'], message)
-          # end
-        end
+    case event[0].type
+    when "postback"
+      client.reply_message(event['replyToken'], sticker_list("thanks"))
+      client.reply_message(event['replyToken'], "#{events[1].data}")
+    # when Line::Bot::Event::Message
+    when "message"
+      client.reply_message(event['replyToken'], menu_template)
+      # case event.type
+      # when Line::Bot::Event::MessageType::Text
+      #     message = menu_template
+      #     client.reply_message(event['replyToken'], message)
+      # when Line::Bot::Event::MessageType::Image
+      #     client.reply_message(event['replyToken'], sticker_list("thanks"))
+      #     # user_id = event["source"]["userId"]
+      #     # client.reply_message(event['replyToken'], sticker_list("thanks"))
+      # end
+        # push(user_id)
+        # @profile = get_line_UserProfile(user_id)
+        # if check_line_user(user_id)
+        #   send_menue
+        # else
+        #   message = send_greeting
+        #   client.reply_message(event['replyToken'], message)
+        # end
       end
     end
 
