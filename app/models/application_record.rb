164e050e59b71ef096b61dd56ab11374ae3223da
ichihara-development-github <haken.ichihara@icloud.com>
2021-04-22 17:26:58 +0900

    corrected_login_action

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,12 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+ protected
+
+  def geocode
+    res = HTTP.get("https://maps.googleapis.com/maps/api/geocode/json?address="+self.address.gsub(" ", "")+"&key=AIzaSyBKnVvlCy9D9nY3-WJ5frpbVzuZK3DIy2Y")
+    response = JSON.parse(res)
+    self.latitude = response["results"][0]["geometry"]["location"]["lat"]
+    self.longitude = response["results"][0]["geometry"]["location"]["lng"]
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -124,12 +124,5 @@ class User < ApplicationRecord
       self.email = email.downcase
     end
 
-    def geocode
-      res = HTTP.get("https://maps.googleapis.com/maps/api/geocode/json?address="+self.address.gsub(" ", "")+"&key=AIzaSyAAWezrBjtug-O85xz84V_TYNCmv1pYxDQ")
-      response = JSON.parse(res)
-      self.latitude = response["results"][0]["geometry"]["location"]["lat"]
-      self.longitude = response["results"][0]["geometry"]["location"]["lng"]
-    end
-
 
 end
