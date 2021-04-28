638fbce930290d58378ebe6886fd8a5ecfb3647f
ichihara-development-github <haken.ichihara@icloud.com>
2021-04-29 00:26:44 +0900

    line_connect

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -82,33 +82,36 @@ class LinebotController < ApplicationController
             client.reply_message(event['replyToken'], default_message)
           end
         end
-        when Line::Bot::Event::Postback
-          if event["postback"]["data"].include?("display_products_stocks")
-            client.reply_message(event['replyToken'], stocks_template)
-          elsif event["postback"]["data"].include?("update_stocks")
-            $product = Product.find(1)
-            client.push_message(@line_id, {"type": "text", "text":"変更後の個数を入力して下さい"})
-          elsif event["postback"]["data"].include?("fuga")
-            client.reply_message(event['replyToken'], sticker_list("thanks"))
-            message = {"type": "text", "text": event["postback"]["data"]}
-            client.push_message(@line_id, message)
-          elsif event["postback"]["data"].include?("check_total_proceeds")
-            message = Postback.check_total_proceeds(@line_user)
-            client.push_message(@line_id, message)
-          elsif event["postback"]["data"].include?("check_baskets")
-            client.push_message(@line_id, baskets_template)
-          elsif event["postback"]["data"].include?("purchase")
-            client.push_message(@line_id,Postback.purchase(event["postback"]["data"].sub("action=purchase&id=","")))
-          end
-        when Line::Bot::Event::AccountLink
-          nonce = Linenonce.find_by(nonce: event["link"]["nonce"])
-          if nonce.user.update(line_id: event["source"]["userId"])
-            client.push_message(user.line_id, hello_message_template(line_user))
-            client.push_message(user.line_id, sticker_list("thanks")
-          else
-            client.push_message(user.line_id, sticker_list("sorry")
-          end
+      when Line::Bot::Event::Postback
+        if event["postback"]["data"].include?("display_products_stocks")
+          client.reply_message(event['replyToken'], stocks_template)
+        elsif event["postback"]["data"].include?("update_stocks")
+          $product = Product.find(1)
+          client.push_message(@line_id, {"type": "text", "text":"変更後の個数を入力して下さい"})
+        elsif event["postback"]["data"].include?("fuga")
+          client.reply_message(event['replyToken'], sticker_list("thanks"))
+          message = {"type": "text", "text": event["postback"]["data"]}
+          client.push_message(@line_id, message)
+        elsif event["postback"]["data"].include?("check_total_proceeds")
+          message = Postback.check_total_proceeds(@line_user)
+          client.push_message(@line_id, message)
+        elsif event["postback"]["data"].include?("check_baskets")
+          client.push_message(@line_id, baskets_template)
+        elsif event["postback"]["data"].include?("purchase")
+          client.push_message(@line_id,Postback.purchase(event["postback"]["data"].sub("action=purchase&id=","")))
         end
-    head :ok
- end
+      when Line::Bot::Event::AccountLink
+        nonce = Linenonce.find_by(nonce: event["link"]["nonce"])
+        if nonce.user.update(line_id: event["source"]["userId"])
+          client.push_message(user.line_id, hello_message_template(line_user))
+          client.push_message(user.line_id, sticker_list("thanks")
+        else
+          client.push_message(user.line_id, sticker_list("sorry")
+        end
+      end
+        head :ok
+    end
+  end
+
+
 end
