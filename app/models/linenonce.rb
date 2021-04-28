4ac70894224c11d26e54b2bf0c662a5e57e8caab
ichihara-development-github <haken.ichihara@icloud.com>
2021-04-28 19:17:46 +0900

    line_connect

--- app/models/linenonce.rb
+++ app/models/linenonce.rb
@@ -1,3 +1,6 @@
 class Linenonce < ApplicationRecordss
   has_one :user, class_name: "user"
+
+  validates :user_id, presence: true, uniqueness: true
+  validates :nonce, presence: true, uniqueness: true
 end
